724020e2a29c3db68f66b87a5f5a2608f4caa6b5
mejia-dev <aaron.mejia8484@gmail.com>
2023-11-07 10:52:53 -0800

    Pass test for add playerScore to scoreTally
    Co-authored-by: Hunter Empey <LizardMak@gmail.com>
    Co-authored-by: Chris Ross Davila <crossdavila@gmail.com>

--- __tests__/scoreTally.test.js
+++ __tests__/scoreTally.test.js
@@ -1,9 +1,18 @@
 import { ScoreTally } from "../src/scoreTally.js";
+import { PlayerScore } from "../src/PlayerScore.js";
 
 describe('ScoreTally', () => {
 
   test('should create instance of scoreTally from constructor', () => {
     const scoreTally = new ScoreTally();
     expect(scoreTally.scores).toEqual({});
+  });
+  
+  test('should add playerScore to scores of the specified playerNumber', () => {
+    const scoreTally = new ScoreTally();
+    const playerScore = new PlayerScore(1,5);
+    scoreTally.trackScore(playerScore);
+    expect(scoreTally.scores[playerScore.playerNumber].currentScore).toEqual(5);
+
   });
 })
