fcfd8ac1cf857b1c2c7867e2181c5ab4b78af4a0
mejia-dev <aaron.mejia8484@gmail.com>
2023-11-07 10:32:18 -0800

    Pass test for adding to currentScore from addScore prototype
    Co-authored-by: Hunter Empey <LizardMak@gmail.com>
    Co-authored-by: Chris Ross Davila <crossdavila@gmail.com>

--- __tests__/PlayerScore.test.js
+++ __tests__/PlayerScore.test.js
@@ -7,4 +7,10 @@ describe('PlayerScore', () => {
     expect(playerScore.playerNumber).toEqual(1);
     expect(playerScore.currentScore).toEqual(2);
   });
+  
+  test('should add to currentScore property from addScore prototype input', () => {
+    const playerScore = new PlayerScore(1,2);
+    playerScore.addScore(2);
+    expect(playerScore.currentScore).toEqual(4);
+  });
 })
